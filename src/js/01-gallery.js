395111254b81fdf42ca5f92a76ceb5c1b95de335
AndreyTus <andrey_tus@ukr.net>
2023-01-11 18:13:36 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(e => {
+    const gallaryLink = document.createElement('a');
+    gallaryLink.className = 'gallery__link';
+    gallaryLink.href = e.original;
+
+    const gallaryImage = document.createElement('img');
+    gallaryImage.className = 'gallery__image';
+    gallaryImage.src = e.preview;
+    gallaryImage.alt = e.description;
+    gallaryImage.title = e.description;
+
+    gallaryLink.append(gallaryImage);
+    items.push(gallaryLink);
+})
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    enableKeyboard: true,
+    captionPosition: 'bottom'
+})
